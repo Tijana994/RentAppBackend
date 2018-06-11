663771606d9fa4fed9b5f7fa0bb6d52f5d9daab6
Marko Tagliavia <max.tagliavia@gmail.com>
2018-06-11 23:14:24 +0200

    pagination done

--- RentApp/Controllers/VehiclesController.cs
+++ RentApp/Controllers/VehiclesController.cs
@@ -50,33 +50,35 @@ namespace RentApp.Controllers
             return Ok(vehicle);
         }
 
-        [HttpGet]
-        [Route("Pagination/{pageNumber}/{pageSize}")]
-        [ResponseType(typeof(Vehicle))]
-        public ICollection<Vehicle> Pagination(int pageNumber, int pageSize)
-        { 
-            return db.Vehicles.GetAll(pageNumber,pageSize).ToList();
-        }
-
 
         [HttpGet]
         [Route("PaginationWithFilter")]
         [ResponseType(typeof(Vehicle))]
-        public ICollection<Vehicle> PaginationWithFilter(int pageNumber, int pageSize, string manuName, string modelName, string year, int fromPrice, int toPrice, string type)
-      {
-            List<Vehicle> firstList = db.Vehicles.GetAll(pageNumber, pageSize).ToList();
-            if(!manuName.Equals("*")) firstList = firstList.Where(x => x.Mark.ToUpper().Equals(manuName.ToUpper())).ToList();
+        public ICollection<Vehicle> PaginationWithFilter(int pageNumber, int pageSize, string manuName, string modelName, string year, int fromPrice, int toPrice, string type, int serviceId)
+        {
+            return db.Vehicles.GetAll(pageNumber, pageSize, manuName, modelName, year, fromPrice, toPrice, type, serviceId).ToList();
+        }
+
+        [HttpGet]
+        [Route("PaginationWithFilterCount")]
+        [ResponseType(typeof(int))]
+        public int PaginationWithFilterCount(int pageNumber, int pageSize, string manuName, string modelName, string year, int fromPrice, int toPrice, string type, int serviceId)
+        {
+            List<Vehicle> firstList = db.Vehicles.GetAll().ToList();
+            if (!manuName.Equals("*")) firstList = firstList.Where(x => x.Mark.ToUpper().Equals(manuName.ToUpper())).ToList();
             if (!modelName.Equals("*")) firstList = firstList.Where(x => x.Model.ToUpper().Equals(modelName.ToUpper())).ToList();
             if (!year.Equals("*")) firstList = firstList.Where(x => x.Year.Equals(year)).ToList();
-            
-            List<Vehicle> returnList = firstList.Where(x => 
+            if (serviceId != -1) firstList = firstList.Where(x => x.ServiceId == serviceId).ToList();
+
+            List<Vehicle> returnList = firstList.Where(x =>
             price(x).Price >= fromPrice &&
             price(x).Price <= toPrice).ToList();
 
-            if (type.Equals("All")) return returnList;
+            if (type.Equals("All")) return returnList.Count();
             else
-            { var t = db.TypeOfVehicles.FirstOrDefault(x => x.Name.Equals(type));
-                return returnList.Where(x => x.TypeOfVehicleId == t.Id).ToList();
+            {
+                var t = db.TypeOfVehicles.FirstOrDefault(x => x.Name.Equals(type));
+                return returnList.Where(x => x.TypeOfVehicleId == t.Id).ToList().Count();
             }
         }
 
